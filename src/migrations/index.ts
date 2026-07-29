b00ce8c0f05a5396ba8af0ded4be57b3d18873af
Amit Dhiman <amitdhiman5086@gmail.com>
2026-07-29 15:54:17 +0530

    chore: upgrade chaipro to v0.2.6, update initial migration, and remove prefix field from payload types

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20260728_162121_initial from './20260728_162121_initial';
+import * as migration_20260729_101652_initial from './20260729_101652_initial';
 
 export const migrations = [
   {
-    up: migration_20260728_162121_initial.up,
-    down: migration_20260728_162121_initial.down,
-    name: '20260728_162121_initial'
+    up: migration_20260729_101652_initial.up,
+    down: migration_20260729_101652_initial.down,
+    name: '20260729_101652_initial'
   },
 ];

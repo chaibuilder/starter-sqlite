e3030c14f2d49dd018bd7003d1f2bd31bb8941d4
Amit Dhiman <amitdhiman5086@gmail.com>
2026-08-21 16:51:02 +0530

    chore: upgraded chai pro package

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -176,6 +176,7 @@ export function buildPayloadConfig(overrides: PayloadConfigOverrides = {}) {
               },
               bucket: process.env.BUCKET_NAME!,
               config: {
+                forcePathStyle: true,
                 credentials: {
                   accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
                   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
